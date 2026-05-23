576e963303ac6f7c0541076d2912272d196ad04e
rampragas10 <rampragas10@gmail.com>
2026-05-23 12:39:01 +0545

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,45 @@
+// import "dotenv/config";
+// import app from "./src/app.js";
+// import http from "http";
+// import connectDB from "./src/config/database.js";
+// import { initSocket } from "./src/sockets/server.socket.js";
+// import path from "path";
+// import express from "express";
+// import { fileURLToPath } from "url";
+
+// const __dirname = path.resolve();
+
+// const PORT = process.env.PORT || 8000;
+
+// // make ready for deployment
+// if (process.env.NODE_ENV === "production") {
+//   // app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+//   // app.get("*", (_, res) => {
+//   //   res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+//   // });
+
+//    app.use(express.static(path.join(__dirname, "Frontend/dist")));
+
+//    app.get("*", (_, res) => {
+//      res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+//    });
+// }
+
+// const httpServer = http.createServer(app);
+
+// initSocket(httpServer);
+
+// connectDB()
+//     .catch((err) => {
+//         console.error("MongoDB connection failed:", err);
+//         process.exit(1);
+//     });
+
+// httpServer.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
 import "dotenv/config";
 import app from "./src/app.js";
 import http from "http";
@@ -5,38 +47,33 @@ import connectDB from "./src/config/database.js";
 import { initSocket } from "./src/sockets/server.socket.js";
 import path from "path";
 import express from "express";
-import { fileURLToPath } from "url";
 
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 8000;
 
+try {
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-// make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  // app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-  // app.get("*", (_, res) => {
-  //   res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  // });
-
-   app.use(express.static(path.join(__dirname, "Frontend/dist")));
-
-   app.get("*", (_, res) => {
-     res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
-   });
-}
-
-const httpServer = http.createServer(app);
-
-initSocket(httpServer);
-
-connectDB()
-    .catch((err) => {
-        console.error("MongoDB connection failed:", err);
-        process.exit(1);
+    app.get("*", (_, res) => {
+      res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
     });
+  }
 
-httpServer.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  const httpServer = http.createServer(app);
+
+  initSocket(httpServer);
+
+  connectDB()
+    .then(() => {
+      httpServer.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error("MongoDB connection failed:", err);
+    });
+} catch (err) {
+  console.error("SERVER ERROR:", err);
+}
